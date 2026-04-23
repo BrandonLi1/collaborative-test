57b794fdf87922c0073dff9a7032e6025c430cde
BT_C09_10 <mobileappdev@bths.edu>
2026-04-23 13:36:25 -0400

    Initial Commit

---
+++ task32/task32App.swift
@@ -0,0 +1,17 @@
+//
+//  task32App.swift
+//  task32
+//
+//  Created by Student on 4/23/26.
+//
+
+import SwiftUI
+
+@main
+struct task32App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
